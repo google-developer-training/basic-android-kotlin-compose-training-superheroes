4f5821bd22ef8d6f61ba208126ff616b8e4db33f
Android Dev <androidedu@google.com>
2024-03-05 15:47:38 -0800

    Edge to Edge new api (#16)

--- app/src/main/java/com/example/superheroes/MainActivity.kt
+++ app/src/main/java/com/example/superheroes/MainActivity.kt
@@ -28,6 +28,7 @@ import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.activity.enableEdgeToEdge
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.tooling.preview.Preview
 import com.example.superheroes.model.HeroesRepository
@@ -36,6 +37,7 @@ import com.example.superheroes.ui.theme.SuperheroesTheme
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
         setContent {
             SuperheroesTheme {
                 // A surface container using the 'background' color from the theme
