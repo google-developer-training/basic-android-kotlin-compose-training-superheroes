c820f1b7dbb7583d0b0f1760815e423fe1120d2c
Jose Alcérreca <jalc@google.com>
2024-12-10 15:02:16 +0000

    sdk 35

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -21,12 +21,12 @@ plugins {
 
 android {
     namespace = "com.example.superheroes"
-    compileSdk = 34
+    compileSdk = 35
 
     defaultConfig {
         applicationId = "com.example.superheroes"
         minSdk = 24
-        targetSdk = 34
+        targetSdk = 35
         versionCode = 1
         versionName = "1.0"
 
